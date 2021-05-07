1c31933e669e13644bf3e54fa0f7a3b2a63c441f
Fernando Ribeiro Salvador de Sá <bra_fernando@hotmail.com>
2021-05-07 00:36:15 -0300

    teste

--- APITemperatura.Testes/TestesConversorTemperatura.cs
+++ APITemperatura.Testes/TestesConversorTemperatura.cs
@@ -6,11 +6,9 @@ namespace APITemperatura.Testes
     public class TestesConversorTemperatura
     {
         [Theory]
-        [InlineData(32, 0)]
         [InlineData(86, 30)]
         [InlineData(90.5, 32.5)]
         [InlineData(212, 100)]
-        [InlineData(0, -17.78)]
         public void TestarConversaoTemperatura(
             double fahrenheit, double celsius)
         {
